e72b083388e360200151fc157c4a1db365db1af1
Sankhlavinay <109958392+Sankhlavinay@users.noreply.github.com>
2024-04-10 17:08:21 +0000

    First Commit of Blog App

---
+++ BlogApp/src/components/index.js
@@ -0,0 +1,34 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from './Container/Container'
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button"
+import Input from './Input'
+import Login from "./Login";
+import Select from "./Select"
+import PostCard from "./PostCard"
+import Signup from './Signup'
+import RTE from "./RTE";
+import PostForm from './Post-Form/PostForm'
+import AuthLayout from './AuthLayout'
+import Home from '../pages/Home'
+
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    Login,
+    Select,
+    PostCard,
+    Signup,
+    RTE,
+    PostForm,
+    AuthLayout,
+    Home
+}
